17f9790d432b3c6d04d043071353b567ed3dbdf5
William Swanson <swansontec@gmail.com>
2017-05-08 03:47:47 -0700

    Create an empty app component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+/**
+ * This is the settings control panel.
+ */
+export class App extends Component {
+  render () {
+    return (
+      <View style={{ padding: 20 }}>
+        <Text>Words</Text>
+      </View>
+    )
+  }
+}
